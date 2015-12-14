512c542bba4715feacf47f635ede56b6bb8a800b
haretaro <frontend.1000@gmail.com>
2015-12-15 05:25:29 +0900

    検索結果の画像を保存できるようになった

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import collection.JavaConversions._
+import scala.io.Source
+import java.net.URL
+import java.io.File
+import sys.process._
 
 object Main extends App{
 
   val driver = new HtmlUnitDriver
-  driver.get("https://google.com/search?q=hoge")
-  println(s"title: ${driver.getTitle()}")
+  driver.get("https://google.com/search?q=hoge&tbm=isch")
+  val imgs = driver.findElementsByTagName("img")
+    .map(img => img.getAttribute("src"))
+    .zipWithIndex
+    .foreach{case (url,index) => new URL(url) #> new File(index+".jpg")!!}
 
 }
