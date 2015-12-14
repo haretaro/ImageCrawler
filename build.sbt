1fe7ed89bdbe0eae56918c66559b933e11680d69
haretaro <frontend.1000@gmail.com>
2015-12-15 04:39:19 +0900

    GETできた

--- build.sbt
+++ build.sbt
@@ -1,1 +1,5 @@
 scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.44.0"
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,9 @@
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+
 object Main extends App{
-  println("hello")
+
+  val driver = new HtmlUnitDriver
+  driver.get("https://google.com/search?q=hoge")
+  println(s"title: ${driver.getTitle()}")
+
 }
